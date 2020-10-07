ecab5823911c3dfb9eb18f43bd0c50a98e67397e
spaceCampBoy <260101@via.dk>
2020-10-07 17:03:07 +0200

    adding modules to gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,25 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+    if (stage === "build-html") {
+      actions.setWebpackConfig({
+        module: {
+          rules: [
+            {
+              test: 'leaflet',
+              use: loaders.null(),
+            },
+            {
+              test: 'react-leaflet',
+              use: loaders.null(),
+            },
+            {
+              test: 'react-loading-overlay',
+              use: loaders.null(),
+            },
+          ],
+        },
+      })
+    }
+  }
